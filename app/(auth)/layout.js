b5c776e7fdd9c4459d07ffa8516dc7b81529787e
Daniyal Farooq <daniyal@taamsoft.com>
2025-07-16 21:25:20 +0500

    fix-2

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,9 +1,9 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { options } from "../api/auth/[...nextauth]/route";
 
 export default async function AuthLayout({ children }) {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(options);
   if (session) {
     redirect("/");
   }
--- components/Login.jsx
+++ components/Login.jsx
@@ -99,7 +99,7 @@ const Login = () => {
     
       };
   return (
-    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:m-0">
       <div className="md:w-1/3 max-w-sm">
         {/* <img
           src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
